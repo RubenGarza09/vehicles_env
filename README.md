321b41389a8938ba2b4777c26bd44ad326e06d0e
RubenGarza09 <rubengarzalau@gmail.com>
2025-02-20 12:40:46 -0600

    Correccion 3 20_02_25

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # vehicles_env
+Este programa despliega una página web que carga los datos obtenidos a partir de un archivo .csv, el cual contiene información acerca de las ventas de vehículos en Estados Unidos.
+A partir de dicha información, crea gráficos de tipo histograma y dispersión cuando se presionan los botones correspondientes.
--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
+st.header('Ventas de vehículos')
 car_data = pd.read_csv('vehicles_us.csv')
 hist_button = st.button('Construir histograma')
 
@@ -14,3 +15,15 @@ if hist_button:
 
     # Mostrar un gráfico plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
+
+disp_button = st.button('Construir gráfico de dispersión')
+
+if disp_button:
+    # Escribir un mensaje
+    st.write('Creación de gráfico de dispersión para el conjunto de datos de anuncions de venta de vehículos')
+
+    # Crear el gráfico de dispersión
+    fig_1 = px.scatter(car_data, x="odometer", y="price")
+
+    # Mostrar un gráfico plotly interactivo
+    st.plotly_chart(fig_1)
